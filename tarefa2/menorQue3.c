f3a647d53808a7be87c0bc28eac6e83c45e69ea6
Vct-Jully <124018755+Vct-Jully@users.noreply.github.com>
2023-07-16 17:17:29 +0000

    Create meenorQue3.c

---
+++ tarefa2/menorQue3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int conjunto[3]= {}, i;
+   scanf("%d\n%d\n%d", &conjunto[0], &conjunto[1], &conjunto[2]);
+  int menor = conjunto[0];
+
+  for(i=0; i<3; i++){
+    if(menor>conjunto[i]){
+      menor = conjunto[i];
+    } 
+  }
+printf("%d", menor);
+    
+	return 0;
+	
+}
---
+++ tarefa2/merceariaDoAmbrosio.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int tarefa1() {
+    double codigo, quant, valorT;
+    scanf("%lf\n%lf", &codigo, &quant);
+
+if(codigo>0 && codigo<=4){
+    if(codigo == 1){
+        valorT = (quant * 5.30);
+    }
+    else if(codigo == 2){
+    valorT= (quant*6.00);
+    }
+    else if(codigo == 3){
+    valorT= (quant*3.20);
+    }
+    else if(codigo == 4){
+    valorT= (quant*2.50);
+    }
+    }
+	
+	if(valorT >= 40 || quant >= 15){
+	    valorT= (valorT - (valorT*0.15));
+    printf("R$ %.2lf", valorT);
+	} else{
+    printf("R$ %.2lf", valorT);
+  }
+  return 0;
+}
